1592606c6afd4055aad4c9cbb737f1a7a1bd0047
aauullliiaa <auliaprtwi15@gmail.com>
2024-12-01 21:14:53 +0800

    update navbar

--- dosen/dosen.php
+++ dosen/dosen.php
@@ -100,75 +100,7 @@ $dosen = retrieve("SELECT * FROM daftar_dosen");
 </head>
 <header>
   <!-- Navbar -->
-  <nav class="navbar navbar-expand-lg shadow-sm fixed-top bg-navbar">
-    <div class="container">
-      <a class="navbar-brand" href="index.php#home">
-        <img src="../src/images/logo kampus.png" alt="Logo" width="40px" />
-      </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#home" role="button" data-bs-toggle="dropdown"
-              aria-expanded="false">
-              Home
-            </a>
-            <ul class="dropdown-menu">
-              <li>
-                <a class="dropdown-item" href="index.php#about">About</a>
-              </li>
-              <li>
-                <a class="dropdown-item" href="index.php#kata-sambutan">Kata Sambutan</a>
-              </li>
-              <li>
-                <a class="dropdown-item" href="index.php#alamat">Alamat dan Kontak</a>
-              </li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="mata-kuliah.php">Mata Kuliah</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#home" role="button" data-bs-toggle="dropdown"
-              aria-expanded="false">
-              Dosen
-            </a>
-            <ul class="dropdown-menu">
-              <li>
-                <a class="dropdown-item" href="dosen.php">Daftar Dosen</a>
-              </li>
-              <li>
-                <a class="dropdown-item" href="penelitian.php">Penelitian Dosen</a>
-              </li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="jadwal-kuliah.php">Jadwal Perkuliahan</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#home" role="button" data-bs-toggle="dropdown"
-              aria-expanded="false">
-              Profil
-            </a>
-            <ul class="dropdown-menu">
-              <li>
-                <a class="dropdown-item" href="profile.php">Edit Profil</a>
-              </li>
-              <li>
-                <a class="dropdown-item" href="form-upload-penelitian.php">Upload Penelitian</a>
-              </li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#" onclick="confirmLogout()">Logout</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+  <?php include 'navbar.php'; ?>
   <!-- End of Navbar -->
   <section class="dosen-hero d-flex align-items-center justify-content-center">
     <h1>Dosen</h1>
